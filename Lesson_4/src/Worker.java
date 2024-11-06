341859c1ef26568b8866f35d589450f8f9ef04f7
Egor Kravchenko <61183935+RealEgich@users.noreply.github.com>
2024-11-06 15:08:11 +0300

    Add files via upload

---
+++ Lesson_4/src/Worker.java
@@ -0,0 +1,29 @@
+public class Worker {
+    private String fio;
+    private String job;
+    private String mail;
+    private long phone;
+    private int zp;
+    private int age;
+
+    public Worker(String _fio, String _job, String _mail, long _phone, int _zp, int _age) {
+       fio = _fio;
+       job = _job;
+       mail = _mail;
+       phone = _phone;
+       zp = _zp;
+       age = _age;
+    }
+    public static void main(String[] args) {
+        Worker Worker1 = new Worker("Кравченко Егор Русланович", "Тестировщик" , "egich008@gmail.com", 291998998, 2300, 26);
+        System.out.println(Worker1);
+    }
+
+    @Override
+    public String toString() {
+        return "Сотрудник: " +
+                "ФИО='" + fio + ", Должность='" + job + ", Email='" + mail + ", Телефон=" + phone + ", Зарплата=" + zp + ", Возраст=" + age;
+    }
+}
+
+
